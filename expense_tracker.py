997ba3d798760090e95d244c36f8480fafdd48d4
jms020524 <jms020524@naver.com>
2025-02-12 22:03:54 +0900

    second commit

--- expense_tracker.py
+++ expense_tracker.py
@@ -17,8 +17,14 @@ def load_expense(filename="expense.json") :
 # Add a new expense
 def add_expense(description, amount) :
     expenses = load_expense()
+
+    if len(expenses) == 0 :
+        expense_id = 1
+    else :
+        expense_id = expenses[-1]['id'] + 1
+
     expense = {
-        "id" : len(expenses) + 1,
+        "id" : expense_id,
         "date" : datetime.now().strftime("%Y-%m-%d"),
         "description" : description,
         "amount" : amount
@@ -35,6 +41,31 @@ def list_expenses() :
     for li in expenses :
         print(f"{li['id']}\t{li['date']}\t{li['description']}\t\t${li['amount']}")
 
+# View summary of all expenses
+def show_summary() :
+    expenses = load_expense()
+    if not expenses :
+        print("No expenses found")
+        return
+
+    summary = 0
+    for li in expenses :
+        summary += int(li['amount'])
+    print(f"Total expenses: ${summary}")
+
+# Remove selected expense
+def delete_expense(id) :
+    expenses = load_expense()
+    
+    for li in expenses :
+        if li['id'] == int(id) :
+            expenses.remove(li)
+            print("Expense deleted successfully")
+            save_expense(expenses)
+            return
+        
+    print(f"Expense with ID: {id} is not found")
+
 
 def main() :
     
@@ -50,12 +81,24 @@ def main() :
     # Create 'list' subparser
     list_parser = subparsers.add_parser("list", help="List all expenses")
 
+    # Create 'summary' subparser
+    summary_parser = subparsers.add_parser("summary", help="View summary of all expenses")
+
+    # Create 'delete' subparser
+    delete_parser = subparsers.add_parser("delete", help="Remove selected expense")
+    delete_parser.add_argument("--id", required=True)
+
     args = parser.parse_args()
 
     if args.command == "add" :
         add_expense(args.description, args.amount)
     elif args.command == "list" :
         list_expenses()
+    elif args.command == "summary" :
+        show_summary()
+    elif args.command == "delete" :
+        delete_expense(args.id)
+
 
 if __name__ == "__main__" :
     main() 
