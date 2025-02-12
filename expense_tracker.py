808d63a3222d904ef210db44f18aaa538e7bab2a
jms020524 <jms020524@naver.com>
2025-02-12 20:59:28 +0900

    Initial commit

---
+++ expense_tracker.py
@@ -0,0 +1,60 @@
+import json, argparse
+from datetime import datetime
+
+# Write Python object to JSON
+def save_expense(expense, filename="expense.json") :
+    with open(filename, "w") as file :
+        json.dump(expense, file)
+
+# Read JSON to Python
+def load_expense(filename="expense.json") :
+    try :
+        with open(filename, "r") as file :
+            return json.load(file)
+    except :
+        return []
+
+# Add a new expense
+def add_expense(description, amount) :
+    expenses = load_expense()
+    expense = {
+        "id" : datetime.now().timestamp(),
+        "date" : datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        "description" : description,
+        "amount" : amount
+    }
+
+    expenses.append(expense)
+    save_expense(expenses)
+    print(f"Expense added successfully (ID: {expense.id})")
+
+# List all expenses
+def list_expenses() :
+    expenses = load_expense()
+    for li in expenses :
+        print("%d\t%s\t%s\t$%d".format(li.id, li.date, li.description, li.amount))
+
+
+def main() :
+    
+    # Create top-level parser
+    parser = argparse.ArgumentParser(description="Expense Tracker Program")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # Create 'add' subparser
+    add_parser = subparsers.add_parser("add", help="Add a new expense")
+    add_parser.add_argument("--description", required=True)
+    add_parser.add_argument("--amount", required=True)
+
+    # Create 'list' subparser
+    list_parser = subparsers.add_parser("list", help="List all expenses")
+
+    args = parser.parse_args()
+
+    if args.command == "add" :
+        add_expense(args.description, args.amount)
+    elif args.command == "list" :
+        list_expenses()
+
+if __name__ == "__main__" :
+    main()
