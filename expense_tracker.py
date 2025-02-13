0c4eadc79ca75432d2d30d3fbc9abfb7809efd4b
jms020524 <jms020524@naver.com>
2025-02-13 22:09:30 +0900

    commit

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,4 +1,4 @@
-import json, argparse
+import json, argparse, calendar
 from datetime import datetime
 
 # Write Python object to JSON
@@ -34,6 +34,23 @@ def add_expense(description, amount) :
     save_expense(expenses)
     print(f"Expense added successfully (ID: {expense["id"]})")
 
+# Update selected expense
+def update_expense(id, description=None, amount=None) :
+    expenses = load_expense()
+
+    for li in expenses :
+        if li['id'] == int(id) :
+            if description != None : 
+                li['description'] = description
+            if amount != None : 
+                li['amount'] = amount
+            save_expense(expenses)
+            print(f"Expense with ID: {id} is successfully updated.")
+            return
+        
+    print(f"Expense with ID: {id} is not found.")
+
+
 # List all expenses
 def list_expenses() :
     expenses = load_expense()
@@ -41,17 +58,31 @@ def list_expenses() :
     for li in expenses :
         print(f"{li['id']}\t{li['date']}\t{li['description']}\t\t${li['amount']}")
 
-# View summary of all expenses
-def show_summary() :
+# View summary of expenses
+def show_summary(month=None) :
     expenses = load_expense()
     if not expenses :
         print("No expenses found")
         return
 
+    if month :
+        month = int(month)
+        if not 1 <= month <= 12 :
+            print("Invalid month value")
+            return
+
     summary = 0
     for li in expenses :
-        summary += int(li['amount'])
-    print(f"Total expenses: ${summary}")
+        date_obj = datetime.strptime(li['date'],"%Y-%m-%d")
+        if month is None or date_obj.month == int(month) :
+            summary += int(li['amount'])
+
+    if month :
+        month_name = calendar.month_name[month]
+        print(f"Total expenses for {month_name} : ${summary}")
+    else :
+        print(f"Total expenses : ${summary}")
+
 
 # Remove selected expense
 def delete_expense(id) :
@@ -64,7 +95,7 @@ def delete_expense(id) :
             save_expense(expenses)
             return
         
-    print(f"Expense with ID: {id} is not found")
+    print(f"Expense with ID: {id} is not found.")
 
 
 def main() :
@@ -78,11 +109,18 @@ def main() :
     add_parser.add_argument("--description", required=True)
     add_parser.add_argument("--amount", required=True)
 
+    # Create 'update' subparser
+    update_parser = subparsers.add_parser("update", help="Update a selected expense")
+    update_parser.add_argument("--id", required=True)
+    update_parser.add_argument("--description")
+    update_parser.add_argument("--amount")
+
     # Create 'list' subparser
     list_parser = subparsers.add_parser("list", help="List all expenses")
 
     # Create 'summary' subparser
     summary_parser = subparsers.add_parser("summary", help="View summary of all expenses")
+    summary_parser.add_argument("--month")
 
     # Create 'delete' subparser
     delete_parser = subparsers.add_parser("delete", help="Remove selected expense")
@@ -92,10 +130,12 @@ def main() :
 
     if args.command == "add" :
         add_expense(args.description, args.amount)
+    elif args.command == "update" :
+        update_expense(args.id, args.description, args.amount)
     elif args.command == "list" :
         list_expenses()
     elif args.command == "summary" :
-        show_summary()
+        show_summary(args.month)
     elif args.command == "delete" :
         delete_expense(args.id)
 
